de32a26dd36d067d88cfebbf24e437890c519460
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-12-05 16:28:41 +0200

    x

--- src/redux/operation.js
+++ src/redux/operation.js
@@ -32,7 +32,7 @@ export const addContacts = createAsyncThunk(
   'contacts/addContact',
   async (contact, thunkAPI) => {
     try {
-      const response = await axios.post(`/contacts`,{contact});
+      const response = await axios.post(`/contacts`,contact);
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
